5f94bf169b929efd61657750d93eb5f8cf4e6df0
demonyka <cweranit@gmail.com>
2024-07-19 01:50:57 +0300

    init

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
-import requests
+import telebot
+from telebot import types
+import importlib
 
-url = "https://dom.ufanet.ru/api/v0/skud/shared/12373/open/"
+# Замените 'YOUR_BOT_API_KEY' на ваш ключ API от BotFather
+API_KEY = '7466245568:AAFiodDer1J32IoXR1GxOlGMjX5TU-_2erg'
+bot = telebot.TeleBot(API_KEY)
 
-headers = {
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,et-EE;q=0.6,et;q=0.5",
-    "Connection": "keep-alive",
-    "Dnt": "1",
-    "Host": "dom.ufanet.ru",
-    "Referer": "https://dom.ufanet.ru/office/skud/",
-    "Sec-Ch-Ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": '"Windows"',
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-}
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    item1 = types.KeyboardButton("Садовая 8, п. 1")
+    markup.add(item1)
+    
+    bot.send_message(
+        message.chat.id,
+        "Какую дверь открыть?",
+        reply_markup=markup
+    )
 
-cookies = {
-    "_ga": "GA1.1.2051290483.1721153727",
-    "tmr_lvid": "2e9bc631f30ef8388b6bc063e579aeee",
-    "tmr_lvidTS": "1721153727537",
-    "_ym_uid": "1721153729228819709",
-    "_ym_d": "1721153729",
-    "lhc_per": '{"vid":"3d64d8109f21c5a90405","ex":17222}',
-    "_ga_LC6P9JT7DE": "GS1.1.1721153727.1.1.1721154146.60.0.0",
-    "ack": "8090d0db5225b03b5d8ceae6380bd0c1",
-    "tz": "Europe/Moscow",
-    "_ym_isad": "2",
-    "csrftoken": "mHEYZrm4FYHFHJr7CG79L4y5a0j4e1pLT8lEa9YIHWhpEVL59Qnz98RRCF6eGCvH",
-    "sessionid": "bl8q1ziifwpm7l5pnjbemqdq0qj0kqwu"
-}
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == "Садовая 8, п. 1":
+        openDoor('sadovaya8_1')
+        bot.reply_to(message, f"Вы открыли дверь {message.text}")
+    else:
+        bot.reply_to(message, "Пожалуйста, выберите одну из доступных дверей.")
 
-response = requests.get(url, headers=headers, cookies=cookies)
+def openDoor(door):
+    module_name = f"doors.{door}"
+    module = importlib.import_module(module_name)
+    if hasattr(module, 'open'):
+        module.open()
+    else:
+        print(f"Модуль {module_name} не имеет функции open()")
 
-print(response.status_code)
-print(response.json())
+# Запуск бота
+bot.polling()
